3875f3f0dd909c1486bdb8ef5ee182580bdc19ba
Flamewake <55808510+Flamewake@users.noreply.github.com>
2022-03-30 16:34:06 -0400

    Added Comments
    read em

--- MATLAB Work/EnvrGraph.m
+++ MATLAB Work/EnvrGraph.m
@@ -18,26 +18,28 @@ Vinit = 0;
 delTime = 0.02;  % Taken from AccelCSV code used to record data, polling rate is 20 ms or 50 hz
 
 % Integrating using for Velocity
-Ux = x1c(1,:) * delTime; % Setting the first velocity value
-Vx1 = x1c; % Making Vx1 a list with the same length of x1c
+Ux = x1c(1,:) * delTime;  % Setting the first velocity value
+Vx1 = x1c;  % Making Vx1 a list with the same length of x1c
 for i=1
     Vx1(i) = Ux(i);
 end
+% Making the first value in the list Vx1 into Ux. Not elegant but I dont have another solution
 
 for i=2:7716
     Vx1(i) = Vx1(i-1) + x1c(i) * Dx1(i);
 end
+% Setting the rest of the values in Vx1 into the actual acceleration values. Vf = Vinit + Accel*Time delta
 
 % Vx1 = Vinit + x1c * delTime;
-Vy1 = Vinit + y1c * delTime;
-Vz1 = Vinit + z1c * delTime;
+% Vy1 = Vinit + y1c * delTime;      These arent right lol
+% Vz1 = Vinit + z1c * delTime;
 
-% Displacement
+% Displacement: V=meters/second, so multiplying the Velocity by time delta gives the distance travelled
 Dx1 = Vx1 * delTime;
 Dy1 = Vy1 * delTime;
 Dz1 = Vz1 * delTime;
 
-% Position
+% Position: Displacement = Dfinal - Dinitial. Im fairly certain theres error here.
 Px1 = Dx1;
 for i=2:7716
     Px1(i) = Dx1(i) - Px1(i-1);
