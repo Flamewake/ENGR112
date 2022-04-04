e27c943f9f3cd8f6bb886dc300feb5e2f9e8ad1f
Flamewake <55808510+Flamewake@users.noreply.github.com>
2022-04-03 20:17:57 -0400

    Integrating for Velocity and graphs
    yep

--- MATLAB Work/ElevatorAnalysis.m
+++ MATLAB Work/ElevatorAnalysis.m
@@ -10,5 +10,23 @@ for i=1:902
     timevar(i) = steppy*hindex(i);
 end
 
-% Plot that
+% Plot Acceleration
 plot(timevar,Za)
+
+% Integrating for Velocity
+% Uz = Za(1,:) * steppy;  % Setting the first velocity value
+Vz = Za;  % Making Vz a list with the same length of Za
+for i=1
+    Vz(i) = timevar(i)*0;
+end
+
+ZaN = Za;
+for i=1:902
+    ZaN(i) = Za(i) - 0.964;
+end
+
+for i=2:902
+    Vz(i) = Vz(i-1) + ZaN(i) * steppy;
+end
+
+plot(timevar,Vz)
